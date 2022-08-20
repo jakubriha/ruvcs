8183ea9d4c3214fdddb7363c10c24e33347ddb0c
Jakub Riha <jakubriha@outlook.com>
2022-08-20 09:30:42 +0200

    Hash file content

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::{fs, io, path::Path};
+
+use sha1::{Sha1, Digest};
+
+fn get_hash<P: AsRef<Path>>(path: P) -> io::Result<String> {
+    
+    let mut file = fs::File::open(path)?;
+
+    let mut hasher = Sha1::new();
+
+    io::copy(&mut file, &mut hasher)?;
+
+    let hash = hasher.finalize();
+
+    Ok(format!("{:x}", hash))
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let hex_hash = get_hash(r"E:\Users\jakub\Downloads\bottom.png").expect("File cannot be read");
+    
+    println!("{}", hex_hash);
+
+
+
+
+
 }
