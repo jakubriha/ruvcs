fb80978bce08bb2e4b0bd7cca025186fe3c17b7c
Jakub Riha <jakubriha@outlook.com>
2023-03-04 19:32:41 +0100

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::{self, create_dir_all}, io::{Read, BufReader, self}, path::Path};
+use std::{fs::{self, create_dir_all}, io::{Read, self}, path::Path};
 
 use sha2::{Sha256, Digest};
 
@@ -13,7 +13,7 @@ fn get_hash(content: &[u8]) -> String {
     format!("{:x}", hash)
 }
 
-fn hash_object(object_content: &[u8]) -> io::Result<String> {
+fn save_object(object_content: &[u8]) -> io::Result<String> {
 
     let object_hash = get_hash(&object_content);
 
@@ -23,7 +23,7 @@ fn hash_object(object_content: &[u8]) -> io::Result<String> {
     let mut path = Path::new(r"E:\Users\jakub\Downloads\test\.vcs\objects")
         .join(hash_prefix);
 
-    create_dir_all(path.clone())?;
+    create_dir_all(&path)?;
 
     path.push(hash_suffix);
     
@@ -32,22 +32,20 @@ fn hash_object(object_content: &[u8]) -> io::Result<String> {
     Ok(object_hash)
 }
 
-fn hash_object_from_file<P: AsRef<Path>>(path: P) -> io::Result<String> {
+fn save_object_given_file<P: AsRef<Path>>(path: P) -> io::Result<String> {
     
-    let file = fs::File::open(path)?;
+    let mut file = fs::File::open(path)?;
 
-    let mut reader = BufReader::new(file);
-    
     let mut buffer = Vec::new();
 
-    reader.read_to_end(&mut buffer)?;
+    file.read_to_end(&mut buffer)?;
 
-    hash_object(buffer.as_slice())
+    save_object(buffer.as_slice())
 }
 
 fn main() {
 
-    let hex_hash = hash_object_from_file(r"E:\Users\jakub\Downloads\test.txt").expect("File cannot be read");
+    let hex_hash = save_object_given_file(r"E:\Users\jakub\Downloads\test.txt").expect("File cannot be read");
     
     println!("{}", hex_hash);
 }
